53bc22c59ab6b9aecb3bdc3d3f1ad29f58e403b4
Mohs9n <mohsen.alokaisy@gmail.com>
2023-12-26 20:03:27 +0200

    Section 8

---
+++ API/Data/Migrations/20231226154402_ExtendedUserEntityTake2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedUserEntityTake2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KnowAs",
+                table: "Users",
+                newName: "KnownAs");
+
+            migrationBuilder.RenameColumn(
+                name: "Intrests",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "KnownAs",
+                table: "Users",
+                newName: "KnowAs");
+
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Intrests");
+        }
+    }
+}
