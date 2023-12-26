4d6e43b571186ee1f58e68b5518821b45040f081
Mohs9n <mohsen.alokaisy@gmail.com>
2023-12-26 10:47:43 +0200

    section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
+
+    public AccountController(DataContext context, ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username taken");
+        
+        using var hmac = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return new UserDTO
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+        if (user == null) return Unauthorized("Username does not Exist");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+        for (int i=0;i<computedHash.Length; i++){
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+        return new UserDTO
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,20 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // app.UseCors(builder => builder.AllowAnyHeader().WithOrigins("https://hoppscotch.io"));
-app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader());
+// app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader());
+app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
