a3d576b6218822bdd7b04a7d5cb8e2607b7e352c
Mohs9n <mohsen.alokaisy@gmail.com>
2023-12-28 22:26:37 +0200

    Section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,31 +13,37 @@ public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(DataContext context, ITokenService tokenService)
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
         _tokenService = tokenService;
         _context = context;
+        _mapper = mapper;
     }
 
     [HttpPost("register")]
     public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
     {
-        if (await UserExists(registerDto.Username)) return BadRequest("Username taken");
+        if (await UserExists(registerDto.Username)) 
+            return BadRequest("Username taken");
         
+        var user = _mapper.Map<AppUser>(registerDto);
+
         using var hmac = new HMACSHA512();
-        var user = new AppUser
-        {
-            UserName = registerDto.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
-        };
+
+        user.UserName = registerDto.Username.ToLower();
+        user.PasswordHash = hmac.ComputeHash
+            (Encoding.UTF8.GetBytes(registerDto.Password));
+        user.PasswordSalt = hmac.Key;
+
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         return new UserDTO
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
         };
     }
 
@@ -47,14 +54,16 @@ public class AccountController : BaseApiController
         if (user == null) return Unauthorized("Username does not Exist");
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+        var computedHash = hmac.ComputeHash
+            (Encoding.UTF8.GetBytes(loginDTO.Password));
         for (int i=0;i<computedHash.Length; i++){
             if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
         }
         return new UserDTO
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
         };
     }
 
